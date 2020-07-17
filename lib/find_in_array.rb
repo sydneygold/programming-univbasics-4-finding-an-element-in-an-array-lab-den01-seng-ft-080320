f4c85ed26c5beb49980f86cd3f277873c1a18cf2
Sydney Goldberg <sydney.goldberg@outlook.com>
2020-07-17 17:36:28 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -3,7 +3,7 @@ def find_element_index(array, value_to_find)
   found_value_index = nil
   while counter < array.length do
     puts array[counter] == value_to_find
-    found_value_index = count
+    found_value_index = counter
     counter += 1
   end
   found_value_index
